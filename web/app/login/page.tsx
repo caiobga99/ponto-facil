473abf91407707bc0a1568ac5232723d03d70013
caiobga99 <caiobga99@gmail.com>
2024-10-15 13:33:56 -0300

    web - add login page.

---
+++ web/app/login/page.tsx
@@ -0,0 +1,165 @@
+"use client";
+import { Input, Button } from "@nextui-org/react";
+import { useState } from "react";
+import NextLink from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { signIn } from "next-auth/react";
+import { subtitle, title } from "@/components/primitives";
+import { EyeSlashFilledIcon, EyeFilledIcon } from "@/components/icons";
+
+const schema = z.object({
+  name: z.string().min(1, "Nome é obrigatório"),
+  email: z.string().email("Formato de email inválido"),
+  password: z.string().min(4, "A senha deve ter pelo menos 4 caracteres"),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export default function LoginPage() {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [serverError, setServerError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+  const toggleVisibility = () => setIsVisible((prev) => !prev);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    setLoading(true);
+    setServerError(null);
+    setSuccessMessage(null);
+
+    const result = await signIn("credentials", {
+      ...data,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      setServerError(result.error);
+      setSuccessMessage(null);
+    } else {
+      setSuccessMessage("Logado com sucesso!");
+      setServerError(null);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <form
+      className="w-full h-full flex flex-col gap-5 justify-between"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col text-left justify-center gap-2">
+        <div>
+          <span className="text-3xl lg:text-4xl">Entre com sua&nbsp;</span>
+          <span className={title({ color: "violet", size: "sm" })}>
+            conta&nbsp;
+          </span>
+        </div>
+        <span className={subtitle()}>
+          Faça o login com sua conta para continuar
+        </span>
+      </div>
+
+      <div className="flex flex-col justify-center h-full w-full gap-4 text-left">
+        <div>
+          <Input
+            label="Nome"
+            placeholder="Digite seu nome"
+            variant="underlined"
+            {...register("name")}
+            isInvalid={!!errors.name}
+            size="lg"
+          />
+          {errors.name && (
+            <span className="text-red-500 text-sm mt-1 ml-2">
+              {errors.name.message}
+            </span>
+          )}
+        </div>
+
+        <div>
+          <Input
+            label="Email"
+            placeholder="Digite seu email"
+            variant="underlined"
+            {...register("email")}
+            isInvalid={!!errors.email}
+            size="lg"
+          />
+          {errors.email && (
+            <span className="text-red-500 text-sm mt-1 ml-2">
+              {errors.email.message}
+            </span>
+          )}
+        </div>
+
+        <div className="w-full">
+          <Input
+            label="Senha"
+            placeholder="Digite sua senha"
+            variant="underlined"
+            {...register("password")}
+            endContent={
+              <button
+                aria-label="toggle password visibility"
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
+              >
+                {isVisible ? <EyeSlashFilledIcon /> : <EyeFilledIcon />}
+              </button>
+            }
+            isInvalid={!!errors.password}
+            size="lg"
+            type={isVisible ? "text" : "password"}
+          />
+          {errors.password && (
+            <span className="text-red-500 text-sm mt-1 ml-2">
+              {errors.password.message}
+            </span>
+          )}
+        </div>
+
+        {serverError && (
+          <span className="text-red-500 text-sm mt-1">{serverError}</span>
+        )}
+        {successMessage && (
+          <span className="text-green-500 text-sm mt-1">{successMessage}</span>
+        )}
+
+        <div className="w-full">
+          <Button
+            className="w-full flex items-center justify-center"
+            color="primary"
+            disabled={loading}
+            size="md"
+            type="submit"
+          >
+            {loading ? "Entrando..." : "Entrar"}
+          </Button>
+        </div>
+
+        <div className="w-full text-center">
+          Ainda não possui uma conta? Verifique o{" "}
+          <NextLink
+            className="data-[active=true]:text-primary data-[active=true]:font-large text-primary"
+            href={"/faq"}
+          >
+            FAQ
+          </NextLink>
+        </div>
+      </div>
+    </form>
+  );
+}
